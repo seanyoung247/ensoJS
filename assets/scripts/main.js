403519308a28d3ca7a2423def5723cc713fdfa78
Sean Young <seanyoung247@gmail.com>
2023-06-01 10:32:21 +0100

    feat: Adds a modal show button

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,11 @@
 (() => {
+    const showModalBtn = document.getElementById('show-modal-btn');
     const testModal = document.getElementById('test-modal');
     const modalColorBtn = document.getElementById('modal-color-btn');
+
+    showModalBtn.addEventListener('click', () => {
+        testModal.show = true;
+    });
 
     modalColorBtn.addEventListener('click', () => {
         testModal.classList.toggle('colored');
