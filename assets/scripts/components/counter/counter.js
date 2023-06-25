a5b84190376b5c7c0f258dcf2c4803ee98700403
sean <seanyoung247@gmail.com>
2023-06-25 23:30:25 +0100

    feat: Adds counter component

---
+++ assets/scripts/components/counter/counter.js
@@ -0,0 +1,20 @@
+
+import Enso from "../enso/enso.js";
+
+Enso.component({
+
+    tagName: 'enso-counter',
+    attributes: {
+        count: { type: Number, default: 0 }
+    },
+    styles:`
+        .container { display: flex; }
+        .display { flex-grow: 2; }`,
+    template:
+        `<div class="container">
+            <button @click="()=>this.count--">-</button>
+            <div class="display">{{ this.count }}</div>
+            <button @click="()=>this.count++">+</button>
+        </div>`
+
+});
