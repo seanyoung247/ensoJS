228e418e01f05601f3b1bc112ce0a5d56bc0a439
Sean Young <seanyoung247@gmail.com>
2023-07-10 10:10:32 +0100

    feat: Picks up cors values from link

--- assets/scripts/components/enso/enso.js
+++ assets/scripts/components/enso/enso.js
@@ -1,17 +1,21 @@
 
 export { default } from './component.js'
 import { load } from './resources.js';
+export { load };
 
 (()=>{
 
     function loadComponents() {
         const components = document.querySelectorAll('link[data-component="enso"]');
         for (const component of components) {
-            fetch(component.href, {
-                method: 'GET',
-                credentials: 'same-origin',
-                mode: 'cors'
-            })
+            const com = component.getAttribute('crossorigin');
+            const header = (com !== null) ? {
+                    method: 'GET', credentials: 'same-origin', mode: 'cors'
+                } : {
+                    method: 'GET', mode: 'no-cors'
+                };
+
+            fetch(component.href, header)
                 .then(responce => responce.text())
                 .then(data => console.log(data));
         }
