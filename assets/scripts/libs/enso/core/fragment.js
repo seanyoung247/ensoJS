120442f1ede5844b9a6283e84110490cf9d3d891
Sean Young <seanyoung247@gmail.com>
2025-09-18 18:15:40 +0100

    feat: Adds fragment skeleton code

--- assets/scripts/libs/enso/core/fragment.js
+++ assets/scripts/libs/enso/core/fragment.js
@@ -1,10 +1,25 @@
 
-
 class EnsoFragment {
-    #bindings = new Map();
-    #templates = [];
-    #refs = {};
-    #env;
-    
-    constructor(template, anchor) {}
+    #bindings = new Map();  // Bindings in this fragment
+    #template;              // Template for this fragment
+    #parent;                // Parent Component
+    #anchor;                // Comment node defining the fragments DOM position
+    #root;                  // Mounted fragment root node
+
+    constructor(parent, template, anchor) {
+
+    }
+
+    get placeholder() {return "enso:fragment";}
+
+    //// Fragment Lifecycle
+    mount() {
+        
+    }
+
+    markChanged() {}
+
+    update() {}
+
+    unmount() {}
 }
