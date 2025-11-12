e44ba1c740a1b4cf5fac731508a4899340359b83
sean <seanyoung247@gmail.com>
2025-11-12 15:59:08 +0000

    refactor: Updated attribute parser to new Event Class

--- src/templates/parsers/attrParser.js
+++ src/templates/parsers/attrParser.js
@@ -1,22 +1,30 @@
 
 // Part of Enso
 // Licensed under the MIT License, see LICENSE file in root.
+console.log('Loading', import.meta.url);
+
 import { parser } from "../parser.js";
 import { getName, isAttr, addBinding, bindSource } from "./utils.js";
-import { createEffect, createStringTemplate } from "../../core/effects.js";
+import { Effect, Action, compileValue} from "../../core/effects.js";
 
-function createAttrEffect(attr, code) {
-    code = createStringTemplate(code);
-    const fn = createEffect(code);
-    return function (env, { element: el }) {
-        const content = fn.call(this, env);
+
+class AttrEffect extends Effect {
+    #attr;
+    constructor(parent, element, action) {
+        super(parent, element, action);
+        this.#attr = action.data.name;
+    }
+
+    run() {
+        const content = super.run();
+        const el = this.element;
         if (content) {
-            el.setAttribute(attr, (content === true) ? '' : content);
+            el.setAttribute(this.#attr, (content === true) ? '' : content);
         }
         else {
-            el.removeAttribute(attr);
+            el.removeAttribute(this.#attr);
         }
-    };
+    }
 }
 
 // Attribute binding (:<attribute name>) parser
@@ -33,10 +41,12 @@ parser.registerAttr({
     preprocess(def, node, attribute) {
         const name = getName(attribute);
         const binds = new Set();
-        const source = bindSource(attribute.value, binds);
+        const source = compileValue(
+            bindSource(attribute.value, binds)
+        );
         def.addAttribute(
             name,
-            createAttrEffect(name, source),
+            new Action(source, {name}, AttrEffect),
             binds
         );
         node.removeAttribute(attribute.name);
@@ -47,11 +57,13 @@ parser.registerAttr({
 
     process(def, parent, element) {
         if (def.attributes) {
-            for (const attr of def.attributes) {
-                const effect = {element, action: attr.effect};
+            for (const attr of def.attributes) { 
+                //CREATE EFFECT
+                // const action = {element, action: attr.action};
+                const effect = attr.action.createEffect(parent, element);
                 // Attach effect to all bindings
                 for (const bind of attr.binds) {
-                    addBinding(parent, bind, effect);
+                    addBinding(parent, bind, effect); //<-- EFFECT CLASS HERE
                 }
             }
         }
