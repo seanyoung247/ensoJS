4b3cfc14146575410cc23fe617141463f02b8f22
sean <seanyoung247@gmail.com>
2025-12-03 23:03:20 +0000

    feat: Added counter component example

---
+++ examples/counter/counter.component.js
@@ -0,0 +1,49 @@
+
+import Enso, { css, html, attr } from 'enso';
+
+
+Enso.component('enso-counter', {
+    watched: {
+        count: attr(0)
+    },
+    styles: css`
+        div {
+            display: flex;
+            justify-content: space-between;
+            padding: 2px;
+            border-radius: 12px / 50%;
+            border: 1px solid black;
+            width: 100%;
+        }
+        button {
+            color: lightgrey;
+            background-color: grey;
+            border-radius: 50%;
+            border: none;
+            width: 20px;
+            aspect-ratio: 1;
+            &:hover {
+                cursor: pointer; 
+            }
+            &:active {
+                color: grey;
+                background-color: lightgrey;
+            }
+        }
+    `,
+    template: html`
+        <div>
+            <button @click="this.dec">-</button>
+            <span>{{ @:count }}</span>
+            <button @click="this.inc">+</button>
+        </div>
+    `,
+    script: {
+        get count() { return this.watched.count; },
+        set count(val) { this.watched.count = val; },
+
+        inc() { this.watched.count++; },
+        dec() { this.watched.count--; },
+        reset() { this.watched.count = 0; },
+    }
+});
--- vitest.config.js
+++ vitest.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
         '.eslintrc.js',
         'src/templates/parsers/parsers.js', // No logic to cover
         'src/core/symbols.js',              // No logic to cover
-        'src/index.js',                     // No logic to cover
+        '*/index.js',                       // No logic to cover
         'version.js',                       // Injects version, don't care
         'examples/**',                      // Don't care about examples
       ],
