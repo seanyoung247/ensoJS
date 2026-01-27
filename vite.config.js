1af93a7caf3a4f199424863bf38ff0a7a30dafda
sean <seanyoung247@gmail.com>
2026-01-27 19:21:08 +0000

    chore: Added source maps

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     minify: 'terser',
+    sourcemap: true,
     lib: {
         entry: path.resolve(__dirname, 'src/index.js'),
         name: 'ensojs',
